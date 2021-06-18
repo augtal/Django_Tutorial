f66b328e39bcb0999e348cfd13f82baf8500b3a2
augtal <augustas.talacka@gmail.com>
2021-06-18 18:43:04 +0300

    Added meetups page and detailed meetups page

--- meetups/views.py
+++ meetups/views.py
@@ -3,4 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {'title': 'A First Meetup', 'location': 'New York', 'slug':'a-first-meetup'},
+        {'title': 'A Second Meetup', 'location': 'Las Vegas', 'slug':'a-second-meetup'},
+        {'title': 'A Third Meetup', 'location': 'California', 'slug':'a-third-meetup'}
+    ]
+    
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
+
+def meetup_details(request):
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is the first meetup!'
+        }
+    return render(request, 'meetups/meetup-detail.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
