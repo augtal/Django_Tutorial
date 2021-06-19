f4f8bb681c949701628fd777fd7dee2f278e58e9
augtal <augustas.talacka@gmail.com>
2021-06-19 17:15:56 +0300

    Fixed bug where user can only enter email once

--- meetups/urls.py
+++ meetups/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('meetups/', views.index, name='meetups-all'), #our domain.com/meetups
     path('meetups/success/', views.confirm_registration, name='meetups-registration-success'),
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetups-details'), #our domain.com/meetups/<dynamic-path-segment>a-first-meetup
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='[meetups-deta]ils'), #our domain.com/meetups/<dynamic-path-segment>a-first-meetup
 ]
